6c46d216a298ab3c892c9bbdd6780385f848bd4c
Moises <msira270997@gmail.com>
2023-03-02 17:09:43 -0400

    Se agrego la tabla de registros de inscripcion

--- Academico/models.py
+++ Academico/models.py
@@ -130,8 +130,19 @@ class RegistroInscripcion(models.Model):
     materias_ids = models.ManyToManyField(Materia, null=True, blank=True)
     fecha_inscripcion = models.DateTimeField(null=True, blank=True)
     # pago_id =
-    estados = [("pendiente", "Pendiente"), ("pagado", "Pagado"), ("inscrito", "Inscrito")]
+    estados = [("pendiente", "Pendiente"), ("pago", "Estado de pago"), ("inscrito", "Inscrito")]
     estado = models.CharField(choices=estados, max_length=15, default="pendiente")
 
     def __str__(self):
         return f'Registro: {self.id} Estudiante: {self.estudiante_id} Materias: {self.materias_ids}'
+
+
+class RegisttroPago(models.Model):
+    id = models.AutoField(primary_key=True)
+    fecha_pago = models.DateTimeField()
+    estudiante_id = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    registro_inscripcion = models.IntegerField()
+    cantidad_pago = models.FloatField()
+
+    def __str__(self):
+        return f'Registro: {self.id} Estudiante: {self.estudiante_id} Inscripcion: {self.registro_inscripcion}'
